1795bd81bdf42c546dd480b8ac6695c0cc8ad811
f3rgS <sienna.ferguson@dawsoncollege.qc.ca>
2024-09-18 14:34:49 +0000

    yay

--- sphere.py
+++ sphere.py
@@ -1,6 +1,6 @@
 radius = float(input("enter your desired radius"))
 def calculatevolume(radius) :
-    volume = (4/3 * 3.14 * radius**3)
+    volume = (1.33 * 3.14 * radius**3)
     return volume
 sphere = calculatevolume(radius)
 print(round(sphere, 0))
